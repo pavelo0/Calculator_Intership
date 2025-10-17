fec5a3cdbc215ecc15dcaa88f8070a319491b1ac
Pavel Melnik <melnikpavel298@gmail.com>
2025-10-17 12:41:51 +0300

    Small updates to change on ESmodules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = "path";
+const HtmlWebpackPlugin = "html-webpack-plugin";
 
-module.exports = {
+export default {
   entry: path.resolve(__dirname, "../src/js/index.js"),
   output: {
     path: path.resolve(__dirname, "../dist"),
---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+
+export default [
+  { ignores: ["dist/**", "node_modules/**"] },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        Element: "readonly",
+        localStorage: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+    rules: {
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
