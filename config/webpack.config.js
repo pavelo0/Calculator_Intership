3c85bebeac7e6da985a3907540184eaaddf6d400
Pavel Melnik <melnikpavel298@gmail.com>
2025-10-17 14:09:46 +0300

    Small updates to change on ESmodules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: path.resolve(__dirname, "../src/js/index.js"),
+  entry: path.resolve(__dirname, "../src/index.js"),
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "bundle.js",
