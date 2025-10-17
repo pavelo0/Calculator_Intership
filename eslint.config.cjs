f041858173069be5b87a8c44b57631d0c54eee55
Pavel Melnik <melnikpavel298@gmail.com>
2025-10-17 12:25:46 +0300

    Small updates

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,27 +1,27 @@
-const js = require("@eslint/js");
+const js = require('@eslint/js');
 
 module.exports = [
-  { ignores: ["dist/**", "node_modules/**"] },
-  js.configs.recommended,
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "commonjs",
-      globals: {
-        window: "readonly",
-        document: "readonly",
-        Element: "readonly",
-        localStorage: "readonly",
-        __dirname: "readonly",
-        module: "readonly",
-        require: "readonly",
-      },
+    { ignores: ['dist/**', 'node_modules/**'] },
+    js.configs.recommended,
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                window: 'readonly',
+                document: 'readonly',
+                Element: 'readonly',
+                localStorage: 'readonly',
+                __dirname: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+            },
+        },
+        rules: {
+            'no-var': 'error',
+            'prefer-const': 'error',
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
     },
-    rules: {
-      "no-var": "error",
-      "prefer-const": "error",
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    },
-  },
 ];
