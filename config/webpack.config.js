28e8688d09282225f48f1ec5ef4d2a8c3ed92762
Pavel Melnik <melnikpavel298@gmail.com>
2025-10-17 12:45:13 +0300

    Small updates to change on ESmodules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,9 @@
-const path = "path";
-const HtmlWebpackPlugin = "html-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   entry: path.resolve(__dirname, "../src/js/index.js"),
